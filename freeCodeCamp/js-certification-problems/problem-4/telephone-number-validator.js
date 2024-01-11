b63e3fae3af3a64bb1bcf9b7bc6c072cfe3444b0
Farhan Chaudhry <mohammed.f.chaudhry@gmail.com>
2024-01-11 15:44:51 +0000

    completed problem 4 JS

---
+++ freeCodeCamp/js-certification-problems/problem-4/telephone-number-validator.js
@@ -0,0 +1,6 @@
+function telephoneCheck(str) {
+    var phoneRegex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s\-]?)\d{3}([\s\-]?)\d{4}$/;
+    return phoneRegex.test(str);
+  }
+  
+  telephoneCheck("555-555-5555");
